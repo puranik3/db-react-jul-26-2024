8a80f65d5923267807965e0565fe0c242d10acf9
Prashanth Puranik <puranik@digdeeper.in>
2024-07-29 16:20:40 +0530

    Fetching workshops

--- 04-react/workshops-app/src/components/WorkshopsList.tsx
+++ 04-react/workshops-app/src/components/WorkshopsList.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import { getWorkshops } from "../services/workshops";
+
 const WorkshopsList = () => {
+    useEffect(() => {
+        // effect function cannot be an async function
+        const helper = async () => {
+            const data = await getWorkshops();
+            console.log(data);
+        };
+
+        helper();
+    }, []);
+
     return (
         <div>
             <h1>List of Workshops</h1>
             <hr />
         </div>
     );
-}
- 
+};
+
 export default WorkshopsList;
--- 04-react/workshops-app/src/index.tsx
+++ 04-react/workshops-app/src/index.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import './services/configureAxios';
+
 import "bootstrap/dist/css/bootstrap.css";
 
 import Application from "./App";
