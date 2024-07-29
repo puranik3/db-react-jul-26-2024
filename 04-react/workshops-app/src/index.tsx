a5cdf259e852d3ca484bd479bb5df7201faadde4
Prashanth Puranik <puranik@digdeeper.in>
2024-07-29 12:37:45 +0530

    started creating components

--- 04-react/workshops-app/src/index.tsx
+++ 04-react/workshops-app/src/index.tsx
@@ -3,25 +3,33 @@ import React from 'react';
 
 // builder - take in the element where you want to create the UI, and the blueprint and creates the UI (DOM manipulations)
 import ReactDOM from 'react-dom/client';
+import Application from './App';
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) as HTMLElement );
 
 // JSX - HTML-like syntax used to define the UI
 root.render(
-  <div>
-    Hello <strong>React</strong>
-  </div>
+  <Application />
 )
+
+// root.render(
+//   <div>
+//     Hello <strong>React</strong>
+//   </div>
+// )
 
 /*
 <div>
     Hello <strong>React</strong>
   </div>
 
-  translates to
+  translated by Babel to
 
   React.createElement(
     'div',
     ...
   )
+
+Babel is run through Webpack (module bundler - creates the bundle.js)
+TypeScript compiler is also run through Webpack
 */
